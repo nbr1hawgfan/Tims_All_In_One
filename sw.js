2ee08c4a4faa488c4ac4e42230fa1421d7e13017
nbr1hawgfan <54566063+nbr1hawgfan@users.noreply.github.com>
2026-07-21 14:37:00 -0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -4,7 +4,7 @@
    Bump CACHE_NAME any time files change to force a refresh.
    ============================================================ */
 
-const CACHE_NAME = 'personal-toolkit-v6';
+const CACHE_NAME = 'personal-toolkit-v7';
 
 const APP_SHELL = [
   './index.html',
